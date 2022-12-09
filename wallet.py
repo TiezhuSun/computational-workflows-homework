a6d58f62626207ef55cad63e8cc5ac7a9b3d8f24
Tiezhu Sun <tiezhusun@qq.com>
2022-12-09 14:36:34 +0100

    update wallet.py for passing tests

--- wallet.py
+++ wallet.py
@@ -6,10 +6,15 @@ class InsufficientAmount(Exception):
 
 class Wallet(object):
     def __init__(self, initial_amount=0):
-        raise NotImplementedError
+        # raise NotImplementedError
+        self.balance = initial_amount
 
     def spend_cash(self, amount):
-        raise NotImplementedError
+        # raise NotImplementedError
+        if self.balance < amount:
+            raise InsufficientAmount
+        self.balance -= amount
 
     def add_cash(self, amount):
-        raise NotImplementedError
+        # raise NotImplementedError
+        self.balance += amount
